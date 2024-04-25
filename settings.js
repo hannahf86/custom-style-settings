85a70e147f5d1359768e630383f140204b4ad7d6
hannahf86 <hannahfeehan.design@gmail.com>
2024-04-25 14:12:57 +0100

    first commit

---
+++ settings.js
@@ -0,0 +1,18 @@
+const toggles = document.querySelectorAll(".settings [type='checkbox']");
+
+const doc = document.documentElement;
+
+// functions
+function updateSiteUi({ name, value }) {
+  console.log({ name, value });
+  return (doc.dataset[name] = value);
+}
+
+// event listeners
+toggles.forEach((toggle) => {
+  toggle.addEventListener("change", (e) => {
+    const { name, checked } = e.target;
+    updateSiteUi({ name, value: checked });
+    localStorage.setItem(name, checked);
+  });
+});
